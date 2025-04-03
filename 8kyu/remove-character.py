c1ada51bf6e3710b4013eaca3dfb7b7e8f906250
nascimentorm <nascimentorm@hotmail.com>
2025-04-03 15:48:32 -0300

    adding remove-character.py and updates the project structure

---
+++ 8kyu/remove-character.py
@@ -0,0 +1,6 @@
+# O objetivo é criar uma função que remova o primeiro e o último caractere de uma string.
+#  Você recebe um parâmetro, a string original. 
+# Você não precisa se preocupar com strings com menos de dois caracteres.
+
+def remove_char(s):
+    return s[1:-1]
