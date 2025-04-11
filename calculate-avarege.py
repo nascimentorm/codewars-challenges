488b3890f62bb2ca09e373a1a88c4b5af109b7e5
Miris <62757011+nascimentorm@users.noreply.github.com>
2025-04-11 11:03:48 -0300

    Delete calculate-avarege.py

--- calculate-avarege.py
+++
@@ -1,9 +0,0 @@
-# Escreva uma função que calcule a média dos números em uma determinada matriz.
-
-# Observação: matrizes vazias devem retornar 0.
-
-
-def find_average(numbers):
-    if not numbers:
-       return 0
-    return sum(numbers) / len(numbers)
